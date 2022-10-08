7834090e19f1a5d21145ee0fc9f7f5a8f0bd26cf
vishnukdas <103939782+vishnukdas@users.noreply.github.com>
2022-10-08 17:11:01 +0530

    first

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,36 +1,36 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
-import { devices } from '@playwright/test';
+import type { PlaywrightTestConfig } from "@playwright/test";
+import { devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-require('dotenv').config();
+require("dotenv").config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
-  testDir: './tests',
+  testDir: "./tests",
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 60 * 2000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 5000,
   },
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [["html", { open: "never" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: process.env.BASE_URL,
@@ -38,31 +38,34 @@ const config: PlaywrightTestConfig = {
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
-    video: 'on',
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
   projects: [
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-      },
-    },
+    // {
+    //   name: "chromium",
+    //   use: {
+    //     ...devices["Desktop Chrome"],
+    //   },
+    // },
 
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-      },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: {
+    //     ...devices['Desktop Firefox'],
+    //   },
+    // },
 
+    // {
+    //   name: 'webkit',
+    //   use: {
+    //     ...devices['Desktop Safari'],
+    //   },
+    // },
     {
-      name: 'webkit',
+      name: "chrome:latest:Windows 10@lambdatest",
       use: {
-        ...devices['Desktop Safari'],
+        viewport: { width: 1280, height: 720 },
       },
     },
 
--- tests/test_1.spec.ts
+++ tests/test_1.spec.ts
@@ -1,17 +1,18 @@
-import { test, expect } from '@playwright/test';
+import { expect } from "@playwright/test";
+import test from "../lambdatest-setup";
 
 const contact = {
-  name: 'Sparsh Kesari',
-  email: 'sparshk@lambdatest.com',
-  phone: '971581112233',
-  subject: 'Booking request for Shady Meadows',
-  message: 'I want to book room at Shady Meadows\n\nRegards,\nSK',
+  name: "Sparsh Kesari",
+  email: "sparshk@lambdatest.com",
+  phone: "971581112233",
+  subject: "Booking request for Shady Meadows",
+  message: "I want to book room at Shady Meadows\n\nRegards,\nSK",
 };
 
-test('read the message in the admin dashboard', async ({ page }) => {
+test("read the message in the admin dashboard", async ({ page }) => {
   const randomNumber = Math.round(Math.random() * 1000000);
 
-  await page.goto('https://automationintesting.online');
+  await page.goto("https://automationintesting.online");
 
   await page.locator('button:has-text("let me hack")').click();
 
@@ -21,30 +22,37 @@ test('read the message in the admin dashboard', async ({ page }) => {
 
   await page.locator('[data-testid="ContactPhone"]').fill(contact.phone);
 
-  await page.locator('[data-testid="ContactSubject"]').fill(`${contact.subject} #${randomNumber}`);
+  await page
+    .locator('[data-testid="ContactSubject"]')
+    .fill(`${contact.subject} #${randomNumber}`);
 
-  await page.locator('[data-testid="ContactDescription"]').fill(contact.message);
+  await page
+    .locator('[data-testid="ContactDescription"]')
+    .fill(contact.message);
 
-  await page.locator('text=Submit').click();
+  await page.locator("text=Submit").click();
 
-  await page.goto('https://automationintesting.online/#/admin');
+  await page.goto("https://automationintesting.online/#/admin");
 
-  await page.locator('[data-testid="username"]').fill('admin');
-  await page.locator('[data-testid="password"]').fill('password');
+  await page.locator('[data-testid="username"]').fill("admin");
+  await page.locator('[data-testid="password"]').fill("password");
   await page.locator('[data-testid="submit"]').click();
 
-  await page.goto('https://automationintesting.online/#/admin/messages');
+  await page.goto("https://automationintesting.online/#/admin/messages");
 
   await page.locator(`text=${randomNumber}`).click();
   for (const contactValue of Object.values(contact)) {
     await expect(page.locator(`text=${contactValue}`).first()).toBeVisible();
-  };
-  await page.locator('text=Close').click();
+  }
+  await page.locator("text=Close").click();
 
   // Remove the message
-  const row = page.locator('.row');
-  await row.locator(':scope', { hasText: randomNumber.toString() }).locator('.fa-remove').click();
+  const row = page.locator(".row");
+  await row
+    .locator(":scope", { hasText: randomNumber.toString() })
+    .locator(".fa-remove")
+    .click();
 
   // Click text=Logout
-  await page.locator('text=Logout').click();
+  await page.locator("text=Logout").click();
 });
